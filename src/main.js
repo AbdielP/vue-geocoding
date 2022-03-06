0cb15fc748c484118afddc5961803d0ac3e190ae
AbdielP <abdiel.pinzonc@gmail.com>
2022-03-05 19:47:00 -0500

    initial dependencies and map working

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import "leaflet/dist/leaflet.css";
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,79 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main__container">
+    <Navbar />
+    <div id="map" class="map"></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+var map = null
+import Navbar from '@/components/Navbar'
+import leaflet from "leaflet"
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Navbar
+  },
+  data () {
+    return {
+      coords: null, // User actual coordinates
+      geoMarker: null // marker for user position
+    }
+  },
+  methods: {
+    getGeolocation: function () {
+      if(this.coords) { // not sure what is this for
+        this.coords = null
+        sessionStorage.removeItem('coords')
+        console.log('emm.. que es esto?')
+        return map.removeLayer(this.geoMarker)
+      }
+      if (sessionStorage.getItem('coords')) {
+        this.coords = JSON.parse(sessionStorage.getItem('coords'));
+        return this.initMap();
+      }
+      navigator.geolocation.getCurrentPosition(this.setCoords, this.getLocError)
+    },
+    setCoords: function (pos) {
+      const setSessionCoords = {
+        lat: pos.coords.latitude,
+        lng: pos.coords.longitude
+      }
+      sessionStorage.setItem('coords', JSON.stringify(setSessionCoords))
+      this.coords = setSessionCoords;
+      this.initMap();
+    },
+    getLocError: function (err) {
+      console.log(err)
+    },
+    initMap: function () {
+      map = leaflet.map("map").setView([this.coords.lat, this.coords.lng], 15);
+      leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
+      const customMarker = leaflet.icon({
+        iconUrl: require("../assets/map-marker-red.svg"),
+        iconSize: [35, 35],
+      });
+      this.geoMarker = leaflet.marker([this.coords.lat, this.coords.lng], {
+        icon: customMarker,
+      }).addTo(map);
+    }
+  },
+  mounted () {
+    this.getGeolocation();
   }
 }
 </script>
+
+<style scoped>
+.main__container {
+  width: 100%;
+  height: 100vh;
+}
+
+.map {
+  width: 100%;
+  height: calc(100% - 56px);
+}
+</style>
